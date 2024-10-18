cf5c3ead8841f8d7b13ebafce029db6b1c08eec9
Pratish Kumar Khandelwal <72383158+pratishpk@users.noreply.github.com>
2024-10-18 20:33:58 +0530

    Create gcd.py

---
+++ gcd.py
@@ -0,0 +1,7 @@
+# Python code to demonstrate the working of gcd()
+# importing "math" for mathematical operations
+import math
+
+# prints 12
+print("The gcd of 60 and 48 is : ", end="")
+print(math.gcd(60, 48))
